41d1008447b250a9c7f87501392cf89c98a62431
Joe Jimenez <75911123+joejimenez-lab@users.noreply.github.com>
2024-11-13 18:48:57 +0000

    implemented forward selection

--- main.cpp
+++ main.cpp
@@ -5,7 +5,59 @@
 using namespace std;
 
 // Function declarations
-void forwardSelection(int totalFeatures);
+// Stub evaluation function
+double evaluateFeatureSet(const vector<int>& featureSet) {
+    // Generate a random score for now
+    return rand() % 100 + 1; // Random score between 1 and 100
+}
+void forwardSelection(int totalFeatures) {
+    cout << "Starting Forward Selection...\n";
+
+    vector<int> selectedFeatures; // Keep track of selected features
+    double bestAccuracy = 0.0;    // Best accuracy so far
+
+    for (int i = 0; i < totalFeatures; ++i) {
+        int bestFeature = -1;   // Store the feature giving the best score
+        double maxAccuracy = 0; // Store the best score in this iteration
+
+        cout << "Evaluating features...\n";
+        for (int feature = 1; feature <= totalFeatures; ++feature) {
+            // Skip features already selected
+            if (find(selectedFeatures.begin(), selectedFeatures.end(), feature) != selectedFeatures.end()) {
+                continue;
+            }
+
+            // Simulate evaluation with a stub function
+            vector<int> tempSet = selectedFeatures;
+            tempSet.push_back(feature);
+            double accuracy = evaluateFeatureSet(tempSet);
+
+            cout << "Using feature(s) { ";
+            for (int f : tempSet) cout << f << " ";
+            cout << "} accuracy is " << accuracy << "%\n";
+
+            if (accuracy > maxAccuracy) {
+                maxAccuracy = accuracy;
+                bestFeature = feature;
+            }
+        }
+
+        if (bestFeature != -1) {
+            selectedFeatures.push_back(bestFeature);
+            bestAccuracy = maxAccuracy;
+            cout << "Feature set { ";
+            for (int f : selectedFeatures) cout << f << " ";
+            cout << "} was best, accuracy is " << bestAccuracy << "%\n";
+        } else {
+            cout << "No further improvements possible.\n";
+            break;
+        }
+    }
+
+    cout << "Finished search!! The best feature subset is { ";
+    for (int f : selectedFeatures) cout << f << " ";
+    cout << "}, which has an accuracy of " << bestAccuracy << "%\n";
+}
 void backwardElimination(int totalFeatures);
 
 int main() {
